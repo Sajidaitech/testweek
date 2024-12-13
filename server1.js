9eae23fc2351e4d0083f3441311554eb263963a7
Sajidaitech <aitechshortvideos@gmail.com>
2024-12-13 15:17:04 +0400

    update cors

--- server1.js
+++ server1.js
@@ -1,31 +1,21 @@
 const express = require("express");
-const app = express();
-const { MongoClient } = require("mongodb");
-const helmet = require("helmet"); // Import helmet
+const MongoClient = require('mongodb').MongoClient;
+const cors = require("cors"); // Import the cors package
 
-// Use Helmet for security headers
+const app = express();
+
+// Use CORS middleware to enable cross-origin requests
 app.use(
-  helmet.contentSecurityPolicy({
-    directives: {
-      defaultSrc: ["'self'"],
-      fontSrc: ["'self'", "data:", "http://localhost:3000"],
-    },
+  cors({
+    origin: "https://sajidaitech.github.io", // Allow requests from your frontend
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allow these methods
+    credentials: true, // Allow cookies or credentials if necessary
   })
 );
 
 app.use(express.json());
 app.set("port", 3000);
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
-  );
 
-  next();
-});
 
 // Function to log the current time
 function logCurrentTime() {
