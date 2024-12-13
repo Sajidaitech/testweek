f51b9f8fbef229e33ad76baad952b5e892b4b0ad
Sajidaitech <aitechshortvideos@gmail.com>
2024-12-13 15:37:03 +0400

    update

--- server1.js
+++ server1.js
@@ -11,12 +11,21 @@ app.use(
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allow these methods
     credentials: true, // Allow cookies or credentials if necessary
   })
-);//cors
-//
+);
 
 app.use(express.json());
 app.set("port", 3000);
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
 
+  next();
+});
 
 // Function to log the current time
 function logCurrentTime() {
